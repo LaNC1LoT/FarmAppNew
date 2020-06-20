a38bd46ed5a7512ac45866689f08688e023a484a
Cholak.AD <Cholak.AD@eka.ru>
2020-06-20 13:38:09 +0300

    Script

---
+++ FarmApp.Domain.Core/Entity/DosageFormType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FarmApp.Domain.Core.Entity
+{
+    public class DosageFormType
+    {
+        public DosageFormType()
+        {
+            Drugs = new HashSet<ApiMethodRole>();
+        }
+        public int Id { get; set; }
+        public string DosageForm { get; set; }
+        public bool? IsDeleted { get; set; }
+        public virtual ICollection<ApiMethodRole> Drugs { get; set; }
+    }
+}
